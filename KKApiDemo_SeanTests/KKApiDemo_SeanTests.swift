026f3645e898a8baba4e657543e2a5367ce377b5
Sean.Yue <sean.yue@seanyues-macbook-pro.local>
2018-12-17 14:14:43 +0800

    Initial Commit

---
+++ KKApiDemo_SeanTests/KKApiDemo_SeanTests.swift
@@ -0,0 +1,34 @@
+//
+//  KKApiDemo_SeanTests.swift
+//  KKApiDemo_SeanTests
+//
+//  Created by Sean.Yue on 2018/12/17.
+//  Copyright © 2018 Sean.Yue. All rights reserved.
+//
+
+import XCTest
+@testable import KKApiDemo_Sean
+
+class KKApiDemo_SeanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
